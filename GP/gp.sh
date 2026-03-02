657ac0013b289073f06a13eed7caf6072932aaff
patmeng <pivanuell@gmail.com>
2026-03-02 20:25:27 +0700

    Update gp.sh

--- GP/gp.sh
+++ GP/gp.sh
@@ -3,7 +3,7 @@ printf '\033]2;helpbot\a'
 while :
 do
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
- google-chrome --user-data-dir=/home/chrome60 https://mamengame.com --start-maximized --no-sandbox --incognito & sleep 1
+ google-chrome --user-data-dir=/home/chrome60 https://mamengame.com --start-maximized --no-sandbox --incognito --autoplay-policy=no-user-gesture-required --disable-blink-features=AutomationControlled & sleep 1
 clear
 sleep 10
 clear
